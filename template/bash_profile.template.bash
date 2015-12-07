c8b7a6256ae2345af931c72b254122d735a11e55
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-12-07 14:24:25 -0500

    Make my theme the default theme

--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -5,10 +5,7 @@ export BASH_IT="$HOME/.bash_it"
 
 # Lock and Load a custom theme file
 # location /.bash_it/themes/
-export BASH_IT_THEME='bobby'
-
-# Your place for hosting Git repos. I use this for private repos.
-export GIT_HOSTING='git@git.domain.com'
+export BASH_IT_THEME='edhurtig'
 
 # Don't check mail when opening terminal.
 unset MAILCHECK
